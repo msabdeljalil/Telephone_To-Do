0d95e0f1554811d246ed953e767c0715df30fdac
Daniel Deutsch <danielallendeutsch@gmail.com>
2014-06-03 11:33:27 -0500

    all new tests passing

--- todo.rb
+++ todo.rb
@@ -1,14 +1,18 @@
 class Task
+  attr_reader :title, :description
+
   def initialize(args)
+    @title = args[:title]
+    @description = args[:description]
   end
 
-  def title
-    "Bull shit"
-  end
+  # def title
+  #   "Bull shit"
+  # end
 
-  def description
-    "Drive all the bulls out of Ireland"
-  end
+  # def description
+  #   "Drive all the bulls out of Ireland"
+  # end
 
   def status
     "incomplete"
