0de5f0eb23ffb916cc0ffd2e146f2e08e00b4579
Daniel Deutsch <danielallendeutsch@gmail.com>
2014-06-03 11:02:52 -0500

    pass first four tests

---
+++ todo.rb
@@ -0,0 +1,20 @@
+class Task
+  def initialize(args)
+  end
+
+  def title
+    "Bull shit"
+  end
+
+  def description
+    "Drive all the bulls out of Ireland"
+  end
+
+  def status
+    "incomplete"
+  end
+
+  def created_at
+    Time.now
+  end
+end
